ae89ebd4bb59f60e204a2887307844a3e85aa811
StefanYosifov <stefyosifov@gmail.com>
2022-04-28 20:59:19 +0300

    Seventh homework [WPF]

---
+++ 7.Homework/02.Homework/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02.Homework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            generateNumbers();
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 5;
+        }
+
+        List<int> randomNums=new List<int>(); 
+        private void generateNumbers()
+        {
+            Random random = new Random();
+            while (randomNums.Count < 5)
+            {
+                int randomNumber = random.Next(1, 26);
+                if (!randomNums.Contains(randomNumber))
+                {
+                    randomNums.Add(randomNumber);
+                }
+            }
+        }
+        int attempts = 0;
+        int wins = 0;
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int getCurrentCell = 0;
+            if (e.ColumnIndex == 0)
+            {
+                getCurrentCell = 1+e.RowIndex;
+            }
+            if(e.ColumnIndex == 1)
+            {
+                getCurrentCell = 6 + e.RowIndex;
+            }
+            if (e.ColumnIndex == 2)
+            {
+                getCurrentCell = 11 + e.RowIndex;
+            }
+            if (e.ColumnIndex == 3)
+            {
+                getCurrentCell = 16 + e.RowIndex;
+            }
+            if (e.ColumnIndex == 4)
+            {
+                getCurrentCell = 21 + e.RowIndex;
+            }
+            if (randomNums.Contains(getCurrentCell))
+            {
+                dataGridView1[e.ColumnIndex,e.RowIndex].Value = "X";
+                wins++;
+                label1.Text = wins.ToString();
+                if (wins >= 5)
+                {
+                    MessageBox.Show($"Congratulations you have won.\nIt took you {attempts} attempts");
+                    return;
+                }
+            }
+            else
+            {
+                dataGridView1[e.ColumnIndex, e.RowIndex].Value = "O";
+            }
+            attempts++;
+            label2.Text =attempts.ToString();
+        }
+
+        
+    }
+}
---
+++ 7.Homework/7.Homework/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7.Homework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        List<int> list = new List<int>();
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int numFromTextBox = 0;
+            int.TryParse(textBox1.Text, out numFromTextBox);
+           
+            if (numFromTextBox < 3 || numFromTextBox > 30)
+            {
+                MessageBox.Show("Input number must be between 3 and 30");
+                return;
+            }
+
+            list.Add(1);
+            list.Add(1);
+            listBox1.Items.Add(1);
+            listBox1.Items.Add(1);
+            
+            
+
+            for(int i = 2; i < numFromTextBox; i++)
+            {
+                list.Add(list[i - 1] + list[i - 2]);
+                listBox1.Items.Add(list[i]);
+            }
+            
+        }
+    }
+}
